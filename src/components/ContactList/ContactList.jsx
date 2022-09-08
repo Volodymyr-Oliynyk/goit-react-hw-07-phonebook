4c0a20f5101855116b111638bd4b7b17f491babe
Volodymyr-Oliynyk <93007917+Volodymyr-Oliynyk@users.noreply.github.com>
2022-09-09 00:48:24 +0300

    fixed-4

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Button } from 'components/common/ButtonStyled';
-import { ContactItem, List } from './ContactListStyled';
+import { ContactItem, List } from './ContactList.styled';
 import {getAllContacts,
   deleteCurrentContact} from 'redux/contacts/contactsOperations'
 import {
